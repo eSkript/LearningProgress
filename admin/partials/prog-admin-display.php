8b24ee7654bdb3f7b6d6f200728c76281fe179dc
unknown <mlorin@ethz.ch>
2017-09-05 12:42:31 +0200

    plugin setting page changed

--- admin/partials/prog-admin-display.php
+++ admin/partials/prog-admin-display.php
@@ -19,62 +19,6 @@
 
     <h2><?php echo esc_html(get_admin_page_title()); ?></h2>
     
-    <p>This plugin is in development, settings might or might not work</p>
-    
-    <p><?php
-        $teststring = "asdf<h1>TEST TEST<p>...</p></h1>qwetpoiuxyv";
-        echo "<br>".$teststring;
-        var_dump(find_html_tag($teststring,"h1",1));
-    ?></p>
-    
-    <form method="post" name="cleanup_options" action="options.php">
-        
-        <?php
-            //Grab all options
-            $options = get_option($this->plugin_name);
-
-            $lecture_progress = $options['lecture_progress'];
-            $debug = $options['debug'];
-        
-            //$debug = 1;
-        
-            //echo "<script>console.log( 'lecture_progress: " .$lecture_progress . ",debug: " .$debug . "' );</script>";
-        ?>
-
-        <?php
-            settings_fields($this->plugin_name);
-            do_settings_sections($this->plugin_name);
-        ?>
-        
-        <?php settings_fields($this->plugin_name); ?>
-    
-        <fieldset>
-            <legend class="screen-reader-text"><span>Show lecture progress</span></legend>
-            <label for="<?php echo $this->plugin_name; ?>-lecture_progress">
-                <input type="checkbox" id="<?php echo $this->plugin_name; ?>-lecture_progress" name="<?php echo $this->plugin_name; ?> [lecture_progress]" value="1" <?php checked($lecture_progress, 1); ?>/>
-                <span><?php esc_attr_e('Show lecture progress', $this->plugin_name); ?></span>
-            </label>
-        </fieldset>
-        
-        <fieldset>
-            <legend class="screen-reader-text"><span>Display chapter length</span></legend>
-            <label for="<?php echo $this->plugin_name; ?>-lchapter_length">
-                <input type="checkbox" id="<?php echo $this->plugin_name; ?>-chapter_length" name="<?php echo $this->plugin_name; ?> [chapter_length]" value="1" <?php checked($lecture_progress, 1); ?>/>
-                <span><?php esc_attr_e('Display chapter length', $this->plugin_name); ?></span>
-            </label>
-        </fieldset>
-        
-        <fieldset>
-            <legend class="screen-reader-text"><span>debug</span></legend>
-            <label for="<?php echo $this->plugin_name; ?>-debug">
-                <input type="checkbox" id="<?php echo $this->plugin_name; ?>-debug" name="<?php echo $this->plugin_name; ?> [debug]" value="1" <?php checked($debug, 1); ?>/>
-                <span><?php esc_attr_e('debug', $this->plugin_name); ?></span>
-            </label>
-        </fieldset>
-        
-    <?php submit_button('Save all changes', 'primary','submit', TRUE); ?>
-    </form>
-    
     <form action="<?php echo admin_url( 'admin-post.php' ); ?>" method="post">
         <input type="hidden" name="action" value="delete_data">
         <input type="hidden" name="_wp_http_referer" value="<?php echo $redirect; ?>">
